42573f16a434e156e9d4f7ee42719f3faec7186b
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-05-22 23:32:45 +0600

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../Shared/NavBar';
+
+const Home = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+        </>
+    );
+};
+
+export default Home;
